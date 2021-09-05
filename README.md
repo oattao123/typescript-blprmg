05babfe2c6aa5b2f32e976f4bc1cebc3e5af58d5
oattao123 <dceriythrrmkic@gmail.com>
2021-09-05 14:58:59 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-blprmg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-blprmg)
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = '<h2>hello world<h2>'
